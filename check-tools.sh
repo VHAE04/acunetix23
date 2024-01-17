4e7f6c54eca5088537ede9a15b936f0baa74e463
VU HOANG ANH <89138607+VHAE04@users.noreply.github.com>
2024-01-17 17:52:45 +0700

    Update check-tools.sh

--- check-tools.sh
+++ check-tools.sh
@@ -46,7 +46,9 @@ typeApp curl
 LAST_VERSION="$(cat /awvs/LAST_VERSION | sed 's/ //g' 2>/dev/null)"
 # <== 读取版本信息
 mv /awvs/awvs.sh /awvs/awvss.sh
-curl https://github.com/VHAE04/acunetix23/raw/main/awvs.sh -O /awvs/awvs.sh
+#curl https://github.com/VHAE04/acunetix23/raw/main/awvs.sh -O /awvs/awvs.sh
+echo "cat /awvs/.hosts >> /etc/hosts" >> /awvs/awvs.sh
+echo "su -l acunetix -c /home/acunetix/.acunetix/start.sh" >> /awvs/awvs.sh
 chmod 777 awvs.sh
 # 获取破解包地址 ==>
 # shellcheck disable=SC2039
